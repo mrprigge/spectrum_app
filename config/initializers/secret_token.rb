2b600559bb7c1cac002aed7ebee0ae72e0adacc8
mrprigge <mrprigge@me.com>
2012-10-03 20:38:46 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpectrumApp::Application.config.secret_token = 'ea4f3a75c4a0b4479f76457e01f292f8f590ea7b03f686607b01768d8e9e0328de24cf8b1eb0bffa3f8d609a9971badbcb13cdb353303dc493ff209b4e59a806'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SpectrumApp::Application.config.session_store :cookie_store, key: '_spectrum_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SpectrumApp::Application.config.session_store :active_record_store
